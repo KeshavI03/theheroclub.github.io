5a2ab1abd14961609af9ab52aff64a1a2a771ea2
Aarya Patel <aarya.patel@gmail.com>
2020-03-25 23:43:28 -0400

    Co-authored-by: sankaran.iyer2003@gmail.com <sankaran.iyer2003@gmail.com> Co-authored-by: Keshav Iyengar <dearkri03@gmail.com> Co-authored-by: shriramdeeboss@gmail.com <shriramdeeboss@gmail.com>

---
+++ home.js
@@ -0,0 +1,61 @@
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+function onload() {
+  initializeClock('clockdiv', 'Jul 11, 2020 09:00:00');
+}
+
+//Number Increment Animation 
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+   let obj = document.getElementById(id),
+    current = start,
+    range = end - start,
+    increment = end > start ? 31 : -1,
+    step = Math.abs(Math.floor(duration / range)),
+    timer = setInterval(() => {
+     current += end > current ? increment : -1;
+     obj.textContent = current;
+     if (current === end) {
+      clearInterval(timer);
+     }
+    }, step);
+  }
+  counter("count3", 0, 35000, 50);
+ });
